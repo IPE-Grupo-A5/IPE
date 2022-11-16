5d024bc01f8f8bdfedf6fec6a097282d228ddba1
Campanha <mpdscamp@gmail.com>
2022-11-16 00:44:22 -0300

    Integração

--- dashboard/models.py
+++ dashboard/models.py
@@ -35,4 +35,5 @@ class Dado(models.Model):
     energia_utilizada_fp = models.PositiveIntegerField()
     potencia_utilizada_p = models.PositiveIntegerField()
     potencia_contratada = models.PositiveIntegerField()
+
     data_tempo = models.DateField()
