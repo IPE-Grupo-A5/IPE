e332e24405838fff26b1cb0cd0937e8ec5cf461c
Marcos-Antunes04 <xboxwackytick@hotmail.com>
2022-11-16 00:15:44 -0300

    Mudança 16/11/2022

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
-
+from django.core.paginator import Paginator
 from dashboard.forms import FormDado
 
 from .models import Dado
@@ -11,7 +11,11 @@ from .models import Dado
 
 @login_required(login_url="/home")
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    data_list = Dado.objects.filter(usuario=request.user)
+    paginator = Paginator(data_list,10)
+    page = request.GET.get('p')
+    data = paginator.get_page(page)
+    return render(request, 'dashboard/dashboard.html',{"dados":data})
 
 
 @login_required(login_url="/home")
@@ -27,6 +31,29 @@ def add_dashboard(request):
         form = FormDado()
 
     return render(request, 'dashboard/add_dashboard.html', {"form": form})
+
+
+@login_required(login_url="/home")
+def update_dashboard(request,pk):
+    data = Dado.objects.get(id=pk)
+    form = FormDado(instance=data)
+    if request.method == "POST":
+        form = FormDado(request.POST, instance=data)
+        if form.is_valid():
+            form.save()
+            return redirect("/dashboard")
+    
+    return render(request, 'dashboard/update_dashboard.html', {"form": form})
+
+
+
+@login_required(login_url="/home")
+def delete_dashboard(request,pk):
+    data = Dado.objects.get(id=pk)
+    if request.method=="POST":
+        data.delete()
+        return redirect("/dashboard")
+    return render(request, 'dashboard/delete_dashboard.html', {"item": data})
 
 
 @login_required(login_url="/home")
